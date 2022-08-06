3cacf530a6a7b86efd27e58f06eb3ee540396d56
Richard W. L. Brehmer <wlb.richard@gmail.com>
2022-08-06 19:00:04 -0300

    first commit

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { LogoProps } from 'types/api';
+import * as S from './styles';
+
+const Logo = ({ alternativeText, url }: LogoProps) => (
+  <S.LogoWrapper src={url} alt={alternativeText} />
+);
+
+export default Logo;
